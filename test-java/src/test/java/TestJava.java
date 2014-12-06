9f4f816ea19467b3f4586b2f781c72d5ac28ba2d
ruizperezmario <ruizperezmario@gmail.com>
2014-12-06 16:46:34 -0300

    Codigo v1

---
+++ test-java/src/test/java/TestJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.java;
+
+/**
+ *
+ * @author Mario
+ */
+public class TestJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int cantNumeros = 1000000;
+        int cantHilos = 10;
+        Calculador calcu = new Calculador();
+        calcu.calcularPrimosCirculares(cantNumeros, cantHilos);
+    }
+}
