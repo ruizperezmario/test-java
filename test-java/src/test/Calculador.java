335dd66bfe846face5f6caae3a8dbd09b374ac45
ruizperezmario <ruizperezmario@gmail.com>
2014-12-06 16:55:22 -0300

    proy v1_0
    con .jar incluido

---
+++ test-java/src/test/Calculador.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mario
+ */
+public class Calculador {
+       //limiteSuperiorAnalizar: Numeros a examinar si son primos circulares o no.
+    public void calcularPrimosCirculares(int limiteSuperiorAnalizar, int cantHilos){    
+        
+        ArrayList<Examinador> listaHilos = new ArrayList();          
+       
+        int contadorHilos = 0;
+        long initialTime;
+        
+        EspacioComun miEspacio = new EspacioComun(cantHilos, limiteSuperiorAnalizar);      
+        Examinador exam;
+        
+        while (contadorHilos < cantHilos){
+            
+            initialTime = System.currentTimeMillis();
+            exam = new Examinador("Hilo " + contadorHilos, miEspacio, initialTime);
+            listaHilos.add(exam);
+            listaHilos.get(contadorHilos).start();
+            
+            contadorHilos++;
+        }
+        
+        ArrayList<Integer> listado = miEspacio.obtenerListado();
+        
+        for(int i=0; i < listado.size(); i++){
+            System.out.println(listado.get(i));
+        } 
+         
+        System.out.println("Cantidad de numeros primos circulares: " + listado.size());
+    
+    }
+}
---
+++ test-java/src/test/EspacioComun.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author Mario
+ */
+public class EspacioComun {
+        
+        ArrayList<Integer> listaPrimosCirculares = new ArrayList<Integer>(); 
+        
+        boolean valueSet = false;
+        int contadorNumeros;
+        
+        int contadorHilos = 0;
+        int cantHilos;
+        int cantMaxNumero;
+        
+        public EspacioComun(int cantHilos, int cantMaxNumero){
+            this.cantHilos = cantHilos;
+            contadorNumeros = 0;
+            this.cantMaxNumero = cantMaxNumero;
+        }
+        
+        synchronized int incrementarContador(){
+            contadorNumeros = contadorNumeros + 1;
+            return contadorNumeros;
+        }
+        
+        synchronized void put(int n) {
+               listaPrimosCirculares.add(n);    
+        }
+        
+        synchronized boolean sePuedeProcesar(){
+            if (contadorNumeros < cantMaxNumero)
+               return true;
+            else
+                return false;
+        }
+        
+         
+        synchronized void incrementarContadorHilos(){
+            contadorHilos++;
+            if (contadorHilos == cantHilos) {
+               notify(); 
+            }
+        }
+        
+        
+        
+        synchronized ArrayList<Integer> obtenerListado() {
+           if (contadorHilos != cantHilos) 
+                       try { wait(); } catch(InterruptedException e){};
+            notify();
+            
+           Collections.sort(listaPrimosCirculares);
+           return listaPrimosCirculares;  
+        }
+
+}
